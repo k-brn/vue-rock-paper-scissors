0e67906ed0347dacd2d02b025acab2b3847a4471
k-brn <108495284+k-brn@users.noreply.github.com>
2024-01-11 11:37:27 -0800

    Add game state functionality

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,89 @@
+<script setup>
+import { ref, computed, onMounted } from "vue";
+
+const wins = ref(0);
+const draws = ref(0);
+const losses = ref(0);
+
+const choice = ref(null);
+const computerChoice = ref(0);
+const verdict = ref(0);
+
+const outcomes = {
+  rock: {
+    rock: "draw",
+    paper: "loss",
+    scissors: "win",
+  },
+  paper: {
+    rock: "win",
+    paper: "draw",
+    scissors: "loss",
+  },
+  scissors: {
+    rock: "loss",
+    paper: "win",
+    scissors: "draw",
+  },
+};
+
+const winPercentage = computed(() => {
+  const total = wins.value + draws.value + losses.value;
+  return total ? (wins.value / total) * 100 : 0;
+});
+
+const play = (c) => {
+  choice.value = c;
+
+  const choices = ["rock", "paper", "scissors"];
+  const random = Math.floor(Math.random() * 3);
+  computerChoice.value = choice[random];
+
+  const outcome = [c][computerChoice.value];
+
+  if (outcome === "win") {
+    wins.value++;
+    verdict.value = "You win!";
+  } else if (outcome === "loss") {
+    losses.value++;
+    verdict.value = "You lose!";
+  } else {
+    draws.value++;
+    verdict.value = "It is a draw!";
+  }
+
+  SaveGame();
+};
+
+const SaveGame = () => {
+  localStorage.setItem("wins", wins.value);
+  localStorage.setItem("draws", draws.value);
+  localStorage.setItem("losses", losses.value);
+};
+
+const LoadGame = () => {
+  wins.value = localStorage.getItem("wins");
+  draws.value = localStorage.getItem("draws");
+  losses.value = localStorage.getItem("losses");
+};
+
+const ResetRound = () => {
+  choice.value = null;
+  computerChoice.value = null;
+  verdict.value = null;
+};
+
+onMounted(() => {
+  LoadGame();
+
+  window.addEventListener("keypress", (e) => {
+    if (e.key === "r") {
+      ResetRound();
+    }
+  });
+});
+</script>
+
 <template>
   <h1 class="text-3xl font-bold underline">Hello world!</h1>
 </template>
